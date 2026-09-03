469f14cd4cfe496032c42ca256965b1c59b75e69
Fida Kamal <55579804+fidakamal@users.noreply.github.com>
2026-09-03 12:36:45 +0600

    Initial commit

---
+++ src/main/java/com/example/ibsbms/IbsbmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ibsbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbsbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbsbmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ibsbms/IbsbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibsbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbsbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
